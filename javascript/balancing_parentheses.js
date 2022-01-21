1352fb9277ff08fdc59e282e1894a826982bf3f0
Ryan Burns <ryankburns@gmail.com>
2022-01-20 21:04:22 -0800

    solved

--- javascript/balancing_parentheses.js
+++ javascript/balancing_parentheses.js
@@ -1,6 +1,18 @@
 function balancingParentheses(string) {
   // type your code here
+  let brackets = [];
+  for (let i = 0; i < string.length; i++) {     
+      if (string[i] === "(") {
+          brackets.push(string[i]);
+      } else if (string[i] === ")") {
+          if (brackets[brackets.length - 1] === "(") brackets.pop();
+          else brackets.push("#");
+      }
+  }
+  return brackets.length;
 }
+
+balancingParentheses("()()()")
 
 if (require.main === module) {
   // add your own tests in here
